31c4e8262e13f99eb2298b5820c1b02668a6471c
Nontawat Noitubtim <139257120+4mGroot@users.noreply.github.com>
2024-05-03 18:10:06 +0700

    lotto

--- testpullreq.js
+++ testpullreq.js
@@ -5,3 +5,6 @@ console.log(x)
     var x = 100
 }
 console.log(x)
+let Lotto = prompt("กรอกเลขที่ต้องการซื้อ")
+document.getElementById("buy").innerHTML = Lotto
+document.getElementById("result").innerHTML = Math.floor(Math.random()*10)
